9ba0cf77df6cbdb184d4501706cb821eb814fe95
MetalLlama55 <beaton@students.avc.edu>
2024-12-11 00:23:20 -0800

    added pink_garnet items to game

--- src/main/java/com/tutorialmod/TutorialMod.java
+++ src/main/java/com/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package com.tutorialmod;
 
+import com.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,8 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems(); //Needed so that items can be added on startup
 	}
 }
---
+++ src/main/java/com/tutorialmod/item/ModItems.java
@@ -0,0 +1,32 @@
+package com.tutorialmod.item;
+
+import com.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems { //Class needed to register new items, must be in item folder
+
+   //To add new items use same syntax
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings())); //Adds the pink_garnet item to the game
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    //Helper method for registering items
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID); ///Debug Log
+
+        //Add items to creative tab group
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET); //Adds pink_garnet to creative tab INGREDIENTS
+            entries.add(RAW_PINK_GARNET);
+        });
+    }
+
+}
