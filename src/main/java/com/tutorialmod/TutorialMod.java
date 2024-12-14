85013ddecea8b3bbfd619e2f20490640cbe30b4a
MetalLlama55 <beaton@students.avc.edu>
2024-12-13 20:19:20 -0800

    gradle and other related files

---
+++ src/main/java/com/tutorialmod/TutorialMod.java
@@ -0,0 +1,32 @@
+package com.tutorialmod;
+
+import com.tutorialmod.block.ModBlocks;
+import com.tutorialmod.component.ModDataComponentTypes;
+import com.tutorialmod.item.ModItemGroups;
+import com.tutorialmod.item.ModItems;
+import com.tutorialmod.util.HammerUsageEvent;
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems(); //Needed so that items can be added on startup
+		ModBlocks.registerModBlocks();
+
+		ModDataComponentTypes.registerDataComponentTypes();
+
+		FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 600);
+
+		PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
+	}
+}
---
+++ src/main/java/com/tutorialmod/datagen/ModModelProvider.java
@@ -0,0 +1,70 @@
+package com.tutorialmod.datagen;
+
+import com.tutorialmod.block.ModBlocks;
+import com.tutorialmod.block.custom.PinkGarnetLampBlock;
+import com.tutorialmod.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.*;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.util.Identifier;
+
+public class ModModelProvider extends FabricModelProvider {
+
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RAW_PINK_GARNET_BLOCK);
+
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PINK_GARNET_ORE);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
+
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.MAGIC_BLOCK);
+
+        //Pink Garnet Blocks
+        BlockStateModelGenerator.BlockTexturePool pinkGarnetPool = blockStateModelGenerator.registerCubeAllModelTexturePool(ModBlocks.PINK_GARNET_BLOCK);
+        pinkGarnetPool.stairs(ModBlocks.PINK_GARNET_STAIRS);
+        pinkGarnetPool.slab(ModBlocks.PINK_GARNET_SLAB);
+        pinkGarnetPool.button(ModBlocks.PINK_GARNET_BUTTON);
+        pinkGarnetPool.pressurePlate(ModBlocks.PINK_GARNET_PRESSURE_PLATE);
+        pinkGarnetPool.fence(ModBlocks.PINK_GARNET_FENCE);
+        pinkGarnetPool.fenceGate(ModBlocks.PINK_GARNET_FENCE_GATE);
+        pinkGarnetPool.wall(ModBlocks.PINK_GARNET_WALL);
+        blockStateModelGenerator.registerDoor(ModBlocks.PINK_GARNET_DOOR);
+        blockStateModelGenerator.registerTrapdoor(ModBlocks.PINK_GARNET_TRAP_DOOR);
+
+        //Changes the lamp model based on if it is on or off
+        Identifier lampOffIdentifier = TexturedModel.CUBE_ALL.upload(ModBlocks.PINK_GARNET_LAMP, blockStateModelGenerator.modelCollector);
+        Identifier lampOnIdentifier = blockStateModelGenerator.createSubModel(ModBlocks.PINK_GARNET_LAMP, "_on", Models.CUBE_ALL, TextureMap::all);
+        blockStateModelGenerator.blockStateCollector.accept(VariantsBlockStateSupplier.create(ModBlocks.PINK_GARNET_LAMP)
+                .coordinate(BlockStateModelGenerator.createBooleanModelMap(PinkGarnetLampBlock.CLICKED, lampOnIdentifier, lampOffIdentifier)));
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(ModItems.PINK_GARNET, Models.GENERATED);
+        itemModelGenerator.register(ModItems.RAW_PINK_GARNET, Models.GENERATED);
+
+        itemModelGenerator.register(ModItems.CAULIFLOWER, Models.GENERATED);
+
+        itemModelGenerator.register(ModItems.CHISEL, Models.HANDHELD);
+
+        itemModelGenerator.register(ModItems.STARLIGHT_ASHES, Models.GENERATED);
+
+        itemModelGenerator.register(ModItems.PINK_GARNET_SWORD, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.PINK_GARNET_PICKAXE, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.PINK_GARNET_AXE, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.PINK_GARNET_SHOVEL, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.PINK_GARNET_HOE, Models.HANDHELD);
+        itemModelGenerator.register(ModItems.PINK_GARNET_HAMMER, Models.HANDHELD);
+
+        itemModelGenerator.registerArmor((ArmorItem) ModItems.PINK_GARNET_HELMET);
+        itemModelGenerator.registerArmor((ArmorItem) ModItems.PINK_GARNET_CHESTPLATE);
+        itemModelGenerator.registerArmor((ArmorItem) ModItems.PINK_GARNET_LEGGINGS);
+        itemModelGenerator.registerArmor((ArmorItem) ModItems.PINK_GARNET_BOOTS);
+    }
+
+}
